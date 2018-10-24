e809200971a91bc5fe6d90c0be2652931460250a
leopord <gameleopord@hotmail.com>
2018-10-24 16:33:39 +0800

    Update config.sh

--- sh/config.sh
+++ sh/config.sh
@@ -33,13 +33,6 @@ fi
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sed -i 's:ZSH_THEME="robbyrussell":ZSH_THEME="gentoo":' ~/.zshrc
 
-# Link bookworm
-ln -sf /usr/bin/com.github.babluboy.bookworm /usr/bin/bookworm
-
-# Extract dictionary file
-tar xvf ./dictionary/Longman_Dictionary_of_Contemporary_English-2.4.2.tar.bz2 -C /usr/share/stardict/dic/
-# tar xvf ./dictionary/Oxford_Advanced_Learner_s_Dictionary-2.4.2.tar.bz2 -C /usr/share/stardict/dic/
-
 # Copy configuration files to Home directory
 pushd conf
 cp -rf `cat list` ~
